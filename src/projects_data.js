3e083b15baf5524d76b9f5aee3d405a272009e04
Danko Beribak <beribakdanko@gmail.com>
2019-11-14 14:04:11 -0800

    images added

--- src/projects_data.js
+++ src/projects_data.js
@@ -1,20 +1,20 @@
 const projects = [
 	{
-		imageUrl: "../../pic1.png", 
+		imageUrl: "../pic1.png", 
 		name: "buddyUp",
 		text: "social app for finding like-minded people",
 		sourceLink: "https://github.com/khoa165/buddyUp",
 		projectLink: "https://buddyup.xyz"
 	},
 	{
-		imageUrl: "../../pic2.PNG", 
+		imageUrl: "../pic2.PNG", 
 		name: "PetNow",
 		text: "    app for renting comfort pets",
 		sourceLink: "https://github.com/khoa165/petNow",
 		projectLink: "https://petnow.herokuapp.com"
 	},
 	{
-		imageUrl: "../../pic3.PNG", 
+		imageUrl: "../pic3.PNG", 
 		name: "Portfolio for a makeup artist",
 		text: "personalized CMS freelance project",
 		sourceLink: "https://github.com/beribak/art-portfolio-jimmy",
