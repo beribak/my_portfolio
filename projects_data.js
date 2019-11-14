07e70a3d3a9981c48dfbd9fb346e71d4c489f175
Danko Beribak <beribakdanko@gmail.com>
2019-11-14 13:41:39 -0800

    dsds

---
+++ projects_data.js
@@ -0,0 +1,25 @@
+const projects = [
+	{
+		imageUrl: "../../pic1.png", 
+		name: "buddyUp",
+		text: "social app for finding like-minded people",
+		sourceLink: "https://github.com/khoa165/buddyUp",
+		projectLink: "https://buddyup.xyz"
+	},
+	{
+		imageUrl: "../../pic2.PNG", 
+		name: "PetNow",
+		text: "    app for renting comfort pets",
+		sourceLink: "https://github.com/khoa165/petNow",
+		projectLink: "https://petnow.herokuapp.com"
+	},
+	{
+		imageUrl: "../../pic3.PNG", 
+		name: "Portfolio for a makeup artist",
+		text: "personalized CMS freelance project",
+		sourceLink: "https://github.com/beribak/art-portfolio-jimmy",
+		projectLink: "https://jimmycortes.herokuapp.com"
+	}
+];
+
+export default projects;
