c3e464d178fa6c93ac17a9579fbe6f10795cb38f
Danko Beribak <beribakdanko@gmail.com>
2019-11-14 14:38:23 -0800

    Updates

---
+++ precache-manifest.241f8e93f0da99482c437c19a10bdb1f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "54e0ff92818bd81c5da3f9f09091a036",
+    "url": "/my-portfolio/index.html"
+  },
+  {
+    "revision": "474f36150b6243572801",
+    "url": "/my-portfolio/static/css/main.251aba31.chunk.css"
+  },
+  {
+    "revision": "9d92a82baee07fe8843e",
+    "url": "/my-portfolio/static/js/2.d49823a5.chunk.js"
+  },
+  {
+    "revision": "474f36150b6243572801",
+    "url": "/my-portfolio/static/js/main.f4d76099.chunk.js"
+  },
+  {
+    "revision": "fdeaa9e7be4744125f9e",
+    "url": "/my-portfolio/static/js/runtime-main.75ff515f.js"
+  },
+  {
+    "revision": "ef8f57b1c788b81f43a8efd4c8cb1045",
+    "url": "/my-portfolio/static/media/pic1.ef8f57b1.png"
+  }
+]);
