3cfc716112f0ef311206a4e2d4cb4912080e1541
Danko Beribak <beribakdanko@gmail.com>
2019-11-15 18:01:20 -0800

    header almost done

--- src/containers/header.jsx
+++ src/containers/header.jsx
@@ -5,7 +5,7 @@ class Header extends Component {
 	render() {
 		return(
 			<div className="header">
-				<h1>Danko Beribak</h1>
+				<h1>Hello, I am <span className="my-color">Danko Beribak.</span><br/> This is my portfolio.</h1>
 				<div className="my-work-btn">
 					<a href="#load">My work</a>
 					<i class="fas fa-arrow-alt-circle-right arrow"></i>
