4d1c742336a28e24ed09681c7ccc7add0ce5a325
Danko Beribak <beribakdanko@gmail.com>
2019-11-17 19:00:07 -0800

    card animation

--- src/containers/project.jsx
+++ src/containers/project.jsx
@@ -10,22 +10,24 @@ class Project extends Component {
 		
 		return(
 			<div className={cl}>
-				<div className="card-text">
-					<h2>{this.props.project.name}</h2>
-					<h4>{this.props.project.text}</h4>
-				</div>
-				<div className="tech-stack">
-					<div className={logo[0]}/>
-					<div className={logo[1]}/>
-					<div className={logo[2]}/>
-					<div className={logo[3]}/>
-				</div>
-				<div className="p-3 mt-2">
-					<div>
-						<a href={this.props.project.sourceLink} target="#" className="source-link" >Source Code</a>
+				<div className="card-content">
+					<div className="card-text">
+						<h2>{this.props.project.name}</h2>
+						<h4>{this.props.project.text}</h4>
 					</div>
-					<div>
-						<a href={this.props.project.projectLink} target="#" className="project-link" >Link</a>
+					<div className="tech-stack">
+						<div className={logo[0]}/>
+						<div className={logo[1]}/>
+						<div className={logo[2]}/>
+						<div className={logo[3]}/>
+					</div>
+					<div className="p-3 mt-2">
+						<div>
+							<a href={this.props.project.sourceLink} target="#" className="source-link" >Source Code</a>
+						</div>
+						<div>
+							<a href={this.props.project.projectLink} target="#" className="project-link" >Link</a>
+						</div>
 					</div>
 				</div>
 			</div>
