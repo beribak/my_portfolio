22256d63af60428e68493d7f2423360d47b5f49e
Danko Beribak <beribakdanko@gmail.com>
2019-12-04 14:20:22 -0800

    card scss chaned

--- src/containers/project.jsx
+++ src/containers/project.jsx
@@ -12,9 +12,14 @@ class Project extends Component {
 				<img className="gif" src={pet_now_gif} alt="Placeholder"/>
 				<div className="project-info">
 					<h2>PetNow</h2>
-					<h4>kakdkaksdkakskakskakskaskaksak</h4>
-					<a href="#" className="link">See Online</a>
-					<a href="#" className="link">Source</a>
+					<h5>AirBnb clone for renting comfort pets</h5>
+					<h5>Build with Ruby on Rails, Javascript and Postgresql</h5>
+					<h5>MVC architectural patern</h5>
+					<h5>CRUD</h5>
+					<div className="links-container">					
+						<a href="#" className="link">See Online</a>
+						<a href="#" className="link">Source</a>
+					</div>
 				</div>
 			</div>
 		);
