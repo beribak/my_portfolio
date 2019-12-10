5a96a09a7c97c00aee6d3c84c0e20b09c4d21926
Danko Beribak <beribakdanko@gmail.com>
2019-12-09 23:55:43 -0800

    tech stack component added with scss file

---
+++ src/assets/stylesheets/tech-card.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class TechCard extends Component {
+
+	render() {
+
+		return(	
+			
+			<div className="tech-card">
+				<h3></h3>
+			</div>				
+			
+		);
+	}
+}
+
+export default TechCard;
--- src/containers/tech.jsx
+++ src/containers/tech.jsx
@@ -1,17 +1,38 @@
 import React, { Component } from 'react';
 
-class About extends Component {
+class TechContainer extends Component {
 
 	render() {
 
 		return(	
 			
 			<div className="tech-container" id="tech">
-				
+				<div className="tech-stack">
+					<h4>Front End</h4>
+					<h5>HTML5</h5>
+					<h5>CSS3-SCSS</h5>
+					<h5>Javascript</h5>
+					<h5>React-Redux</h5>
+				</div>
+				<div className="tech-stack">
+					<h4>Back End</h4>
+					<h5>Ruby on Rails</h5>
+					<h5>Node</h5>
+				</div>
+				<div className="tech-stack">
+					<h4>Database</h4>
+					<h5>Postgresql</h5>
+					<h5>My SQL</h5>
+				</div>
+				<div className="tech-stack">
+					<h4>Deployment</h4>
+					<h5>Heroku</h5>
+					<h5>AWS</h5>
+				</div>
 			</div>				
 			
 		);
 	}
 }
 
-export default About;
+export default TechContainer;
